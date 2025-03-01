93c19452bd3cacc1a36b44a1900898f1b5edfda3
Quickly Egypt <quickly.dev.eg@gmail.com>
2025-03-01 03:09:04 +0200

    api

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Header.css';
+import './Header.css';         
 import { Link } from 'react-router-dom';
 import 'animate.css';
 import { useTranslation } from 'react-i18next';
@@ -7,9 +7,7 @@ import { useTranslation } from 'react-i18next';
 const Header = () => {
   const { t, i18n } = useTranslation();
 
-
   const [heroData, setHeroData] = useState<any>(null);
-
 
   useEffect(() => {
     const requestOptions = {
@@ -21,7 +19,7 @@ const Header = () => {
       .then((response) => response.json())
       .then((result) => {
         if (result.success) {
-
+         
           setHeroData(result.data[0]);
         } else {
           console.error("API returned success = false", result);
@@ -31,57 +29,64 @@ const Header = () => {
   }, []);
 
   return (
-    <>
-      <header
-        style={{
-          direction: i18n.language === 'ar' ? 'rtl' : 'ltr',
-          fontFamily: i18n.language === 'ar'
-            ? '"Cairo", sans-serif'
-            : '"Poppins", serif'
-        }}
-        className="home_header"
-      >
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 col-lg-6 animate__animated animate__fadeIn">
-              <h1>
-                {
-                  heroData &&
-                  (i18n.language === 'ar'
-                    ? heroData.main_title_ar
-                    : heroData.main_title_en
-                  )
-                }
-              </h1>
+    <header
+  
+      style={{
+        direction: i18n.language === 'ar' ? 'rtl' : 'ltr',
+        fontFamily: i18n.language === 'ar'
+          ? '"Cairo", sans-serif'
+          : '"Poppins", serif',
 
-              <h6 className="my-4 fs-4 fw-light">
-                {
-                  heroData &&
-                  (i18n.language === 'ar'
-                    ? heroData.details_ar
-                    : heroData.details_en
-                  )
-                }
-              </h6>
 
-              <div className="d-flex align-items-center">
-                <button
-                  className="btn btn-dark px-4 me-4"
-                  style={{ padding: '14px 28px' }}
+        backgroundImage: heroData ? `url(${heroData.image})` : 'none',
+        backgroundPosition: 'center center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundColor: 'rgba(0, 0, 0, 0.281)',
+        backgroundBlendMode: 'overlay',
+        color: 'white',
+        padding: '133px 0',
+        height: '650px',
+        backgroundAttachment: 'fixed',
+      }}
+      className="home_header animate__animated animate__fadeIn"
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 col-lg-6">
+            <h1>
+              {heroData && (
+                i18n.language === 'ar'
+                  ? heroData.main_title_ar
+                  : heroData.main_title_en
+              )}
+            </h1>
+
+            <h6 className="my-4 fs-4 fw-light">
+              {heroData && (
+                i18n.language === 'ar'
+                  ? heroData.details_ar
+                  : heroData.details_en
+              )}
+            </h6>
+
+            <div className="d-flex align-items-center">
+              <button
+                className="btn btn-dark px-4 me-4"
+                style={{ padding: '14px 28px' }}
+              >
+                <Link
+                  className="text-white text-decoration-none"
+                  to="/products"
                 >
-                  <Link
-                    className="text-white text-decoration-none"
-                    to={'/products'}
-                  >
-                    {t("HomeHeaderBtn")}
-                  </Link>
-                </button>
-              </div>
+                  {t("HomeHeaderBtn")}
+                </Link>
+              </button>
             </div>
           </div>
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   );
 };
 
